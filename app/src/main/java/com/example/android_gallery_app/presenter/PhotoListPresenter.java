04e4a7c77a4d35979503c50564362737e5aad485
Daniel Engelhard <danny.engelhard@gmail.com>
2020-10-18 17:18:03 -0700

    light mvp mods like adding folders and dividing the photolist functionality

---
+++ app/src/main/java/com/example/android_gallery_app/presenter/PhotoListPresenter.java
@@ -0,0 +1,18 @@
+package com.example.android_gallery_app.presenter;
+
+import android.widget.ImageView;
+
+import com.example.android_gallery_app.Photo;
+
+import java.io.IOException;
+import java.util.Date;
+
+public interface PhotoListPresenter {
+    Photo addCaption(String caption);
+    void addPhoto(Photo photo);
+    void deletePhoto(String mCurrentPhotoPath) throws IOException;
+    String findPhotos_second(Date startTimestamp, Date endTimestamp, String keywords, String topLeft, String bottomRight);
+    Photo getPhoto();
+    Photo getPhotoByLocation(String loc);
+    Photo scrollPhotos(Boolean proc);
+}
